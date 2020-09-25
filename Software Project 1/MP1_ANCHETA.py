0f6606f7fede28982d9a5ded3e8802c44fdbe46e
Vash Patrick Ancheta <vashpatrickancheta@gmail.com>
2020-09-25 13:54:55 +0800

    Initial Commit

---
+++ Software Project 1/MP1_ANCHETA.py
@@ -0,0 +1,15 @@
+# Rick's Great Plan for Success
+#  1) Validate date range
+#     [ ] Within date limit
+#     [ ] All inputs are integers
+#     [ ] Valid dates
+#  2) Number crunch
+#     [ ] Compute total days
+#     [ ] Compute weekdays
+#     [ ] Compute leap years
+#     [ ] Compute holidays falling on weekdays
+#         [ ] New Year
+#         [ ] Labor Day
+#         [ ] All Saint's Day
+#         [ ] Christmas
+#  [ ] Compute work days
